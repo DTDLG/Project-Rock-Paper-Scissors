166817f921d051615424756f476590bef748f833
Daniel Torres <dtdlgdesign@gmail.com>
2025-07-01 15:20:59 -0600

    Create game logic pseudocode

---
+++ game-script.js
@@ -0,0 +1,19 @@
+// Rock, Paper, Scissors game logic.
+
+// GET COMPUTER CHOICE
+// Generate a random choice of rock, paper or scissors
+
+// GET USER CHOICE.
+// Ask for user input to get either rock, paper or scissors
+
+// DECLARE PLAYER SCORE VARIABLES.
+
+// PLAY A SINGLE ROUND.
+// Check if computer and player choices are the same.
+// Check player choice and compare to each possible computer choice.
+
+// PLAY ENTIRE GAME (5 ROUNDS).
+// Display current round results.
+// Display total results.
+// Start new round and get user and computer's choice.
+// Repeat for a total of 5 roundsß.
